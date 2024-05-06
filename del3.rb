6a698cd0faa4a4c19dc3c096c8873f751bb73d9a
NoahSaas <151681167+NoahSaas@users.noreply.github.com>
2024-05-06 13:00:38 +0200

    .

--- del3.rb
+++ del3.rb
@@ -1,4 +1,21 @@
 def game()
+    puts("""
+        Välkommen till Nim! Nim är ett strategispel för två spelare med tändstickor (eller andra markörer).
+      
+        **Målet:** Vara den som tar den sista tändstickan.
+      
+        **Så här går spelet till:**
+      
+        1. Välj mellan 3 och 7 högar med tändstickor.
+        2. Turas om att ta bort 1 till 3 tändstickor från valfri hög under din tur.
+        3. Du kan inte ta fler tändstickor än vad som finns i högen.
+        4. Spelet fortsätter tills det bara finns en enda tändsticka kvar.
+        5. Den som tar den sista tändstickan vinner!
+      
+        **Tips:** Försök att lämna din motståndare i en position där de bara kan ta bort den sista tändstickan.
+
+        """)
+
     puts "Vill du spela mot en dator eller en annan spelare, Skriv in dator eller spelare (D/S)"
 
     opponent = gets.chomp.to_s.downcase
@@ -7,25 +24,37 @@ def game()
         opponent = gets.chomp.to_s.downcase
     end
 
+    system("cls")
+
     if opponent == "s"
         puts "Skriv in namn på spelare 1:"
         name_1 = gets.chomp
 
+        system("cls")
+
         puts "Skriv in namn på spelare 2:"
         name_2 = gets.chomp
+
+        system("cls")
     else
         puts "Skriv in namn på spelare 1:"
         name_1 = gets.chomp
 
         name_2 = "Dator"
+
+        system("cls")
     end
 
-    puts "Hur många högar vill du spela med? (3-7)"
+    puts "Spelarna är #{name_1} mot #{name_2}"
+
+    puts "Hur många högar vill ni spela med? (3-7)"
     heaps = gets.chomp.to_i
     while heaps < 3 || heaps > 7
         puts "Välj mellan 3 till 7 högar"
         heaps = gets.chomp.to_i
     end
+    
+    system("cls")
 
     answer = nil
 
@@ -64,36 +93,53 @@ end
 
 def pvp(sticks, heaps, name_1, name_2, starting_player)
     while heaps > 0
+        system("cls")
         puts "#{starting_player}:s tur"
 
         puts "Såhär ser högarna ut: #{sticks}"
-        puts "Från vilken hög vill du ta bort 1-#{heaps}?:"
-        heap_to_remove = gets.chomp.to_i 
+        puts "Från vilken hög vill du ta bort 1-#{sticks.length}?:"
+        heap_to_remove = gets.chomp.to_i
 
-        while heap_to_remove > heaps || heap_to_remove < 1
-            puts "Det finns #{heaps}st högar kvar, från vilken hög vill du ta bort 1-#{heaps}?:"
-            puts "Det finns bara #{heaps} antal högar, du måste ta bort från en av dem"
+        while heap_to_remove > sticks.length || heap_to_remove < 1
+            system("cls")
+            puts "Det finns bara #{sticks.length}st högar kvar, från vilken hög vill du ta bort 1-#{sticks.length}?:"
+            puts "#{sticks}"
             heap_to_remove = gets.chomp.to_i
         end
-        
+        system("cls")
+        puts "Det finns #{sticks[heap_to_remove - 1]}st pinnar i högen"
         puts "Hur många pinnar vill du ta bort ur högen? (1-3)"
         sticks_to_remove = gets.chomp.to_i
 
         while sticks_to_remove > 3 || sticks_to_remove < 1
-            puts "Det finns #{sticks[heap_to_remove]}st pinnar kvar, hur många vill du ta bort (1-3)?:"
+            system("cls")
+            puts "Det finns #{sticks[heap_to_remove - 1]}st pinnar kvar, hur många vill du ta bort (1-3)?:"
             puts "Fel antal pinnar. Man måste ta bort ett ett antal mellan 1-3."
             sticks_to_remove = gets.chomp.to_i
         end
 
         sticks[heap_to_remove - 1] -= sticks_to_remove
 
+        if sticks[heap_to_remove - 1] <= 0
+            sticks.delete_at(heap_to_remove - 1)
+        end
+
         if starting_player == name_1
             starting_player = name_2
         else
             starting_player = name_1
         end
 
-        if sticks <= 0
+        sticks_left = false
+        i = 0
+        while i < sticks.length
+            if sticks[i] > 0
+                sticks_left = true    
+            end
+            i += 1
+        end
+        if sticks_left == false
+            system("cls")
             puts "VINNARER ÄR: #{starting_player}"
             break
         end
@@ -101,25 +147,47 @@ def pvp(sticks, heaps, name_1, name_2, starting_player)
 end
 
 
-def pve(sticks, name_1, name_2, starting_player)
-    while sticks > 0
-        if starting_player != "Dator"
-            puts "#{starting_player}:s tur"
+def pve(sticks, heaps, name_1, name_2, starting_player)
+    while heaps > 0
+        system("cls")
+        puts "#{starting_player}:s tur"
 
-            puts "Det finns #{sticks}st pinnar kvar, hur många vill du ta bort (1-3)?:"
+        if starting_player == "Dator"
+            puts "Såhär ser högarna ut: #{sticks}"
+
+            heap_to_remove = pro_computer(sticks)[0]
+            sticks_to_remove = pro_computer(sticks)[1]
+
+            puts "Datorn tar bort #{sticks_to_remove} från hög #{heap_to_remove}"
+            sleep(4)
+        else
+            puts "Såhär ser högarna ut: #{sticks}"
+            puts "Från vilken hög vill du ta bort 1-#{sticks.length}?:"
+            heap_to_remove = gets.chomp.to_i
+
+            while heap_to_remove > sticks.length || heap_to_remove < 1
+                system("cls")
+                puts "Det finns bara #{sticks.length}st högar kvar, från vilken hög vill du ta bort 1-#{sticks.length}?:"
+                puts "#{sticks}"
+                heap_to_remove = gets.chomp.to_i
+            end
+            system("cls")
+            puts "Det finns #{sticks[heap_to_remove - 1]}st pinnar i högen"
+            puts "Hur många pinnar vill du ta bort ur högen? (1-3)"
             sticks_to_remove = gets.chomp.to_i
 
             while sticks_to_remove > 3 || sticks_to_remove < 1
-                puts "Det finns #{sticks}st pinnar kvar, hur många vill du ta bort (1-3)?:"
+                system("cls")
+                puts "Det finns #{sticks[heap_to_remove - 1]}st pinnar kvar, hur många vill du ta bort (1-3)?:"
                 puts "Fel antal pinnar. Man måste ta bort ett ett antal mellan 1-3."
                 sticks_to_remove = gets.chomp.to_i
             end
-        else
-            sticks_to_remove = rand(1...3)
-            puts "Datorn tog bort #{sticks_to_remove}st pinnar"
         end
+        sticks[heap_to_remove - 1] -= sticks_to_remove
 
-        sticks -= sticks_to_remove
+        if sticks[heap_to_remove - 1] <= 0
+            sticks.delete_at(heap_to_remove - 1)
+        end
 
         if starting_player == name_1
             starting_player = name_2
@@ -127,12 +195,37 @@ def pve(sticks, name_1, name_2, starting_player)
             starting_player = name_1
         end
 
-        if sticks <= 0
+        sticks_left = false
+        i = 0
+        while i < sticks.length
+            if sticks[i] > 0
+                sticks_left = true    
+            end
+            i += 1
+        end
+        if sticks_left == false
+            system("cls")
             puts "VINNARER ÄR: #{starting_player}"
             break
         end
     end
 end
 
+def pro_computer(sticks)
+
+    i = 0
+    while i < sticks.length
+        if sticks[i] <= 4 && sticks[i] > 1
+            heap_to_remove = i
+        end
+        i += 1
+    end
+
+    if 
+        sticks_to_remove = rand(1..3)
+    end
+    
+    return heap_to_remove, sticks_to_remove
+end
 
 game()
