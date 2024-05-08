55660e32343ba11a1feef2c6e8eb5fda5e2f88ec
maxsvensbyNTI2C <max.svensby@elev.ga.ntig.se>
2024-05-08 22:38:16 +0200

    klar med dator och med dokumentation

--- del3.rb
+++ del3.rb
@@ -20,6 +20,7 @@ def game()
 
     opponent = gets.chomp.to_s.downcase
     while opponent != "d" && opponent != "s"
+        god(opponent)
         puts "Svaret måste vara D (dator) eller S (spelare)"
         opponent = gets.chomp.to_s.downcase
     end
@@ -79,8 +80,8 @@ def game()
         else
             pve(sticks, heaps, name_1, name_2, starting_player)
         end
-            
-            
+        
+        
         puts "Vill ni spela igen? (Y/N)"
         answer = gets.chomp.to_s.downcase
         while answer != "n" && answer != "y"
@@ -108,13 +109,13 @@ def pvp(sticks, heaps, name_1, name_2, starting_player)
         end
         system("cls")
         puts "Det finns #{sticks[heap_to_remove - 1]}st pinnar i högen"
-        puts "Hur många pinnar vill du ta bort ur högen? (1-3)"
+        puts "Hur många pinnar vill du ta bort ur högen? (1-#{min(sticks[heap_to_remove - 1], 3)})"
         sticks_to_remove = gets.chomp.to_i
 
         while sticks_to_remove > 3 || sticks_to_remove < 1
             system("cls")
-            puts "Det finns #{sticks[heap_to_remove - 1]}st pinnar kvar, hur många vill du ta bort (1-3)?:"
-            puts "Fel antal pinnar. Man måste ta bort ett ett antal mellan 1-3."
+            puts "Det finns #{sticks[heap_to_remove - 1]}st pinnar kvar, hur många vill du ta bort (1-#{min(sticks[heap_to_remove - 1], 3)})?:"
+            puts "Fel antal pinnar. Man måste ta bort ett ett antal mellan 1-#{min(sticks[heap_to_remove - 1], 3)}."
             sticks_to_remove = gets.chomp.to_i
         end
 
@@ -140,7 +141,7 @@ def pvp(sticks, heaps, name_1, name_2, starting_player)
         end
         if sticks_left == false
             system("cls")
-            puts "VINNARER ÄR: #{starting_player}"
+            puts "VINNAREN ÄR: #{starting_player}"
             break
         end
     end
@@ -173,13 +174,13 @@ def pve(sticks, heaps, name_1, name_2, starting_player)
             end
             system("cls")
             puts "Det finns #{sticks[heap_to_remove - 1]}st pinnar i högen"
-            puts "Hur många pinnar vill du ta bort ur högen? (1-3)"
+            puts "Hur många pinnar vill du ta bort ur högen? (1-#{min(sticks[heap_to_remove - 1], 3)})"
             sticks_to_remove = gets.chomp.to_i
 
             while sticks_to_remove > 3 || sticks_to_remove < 1
                 system("cls")
-                puts "Det finns #{sticks[heap_to_remove - 1]}st pinnar kvar, hur många vill du ta bort (1-3)?:"
-                puts "Fel antal pinnar. Man måste ta bort ett ett antal mellan 1-3."
+                puts "Det finns #{sticks[heap_to_remove - 1]}st pinnar kvar, hur många vill du ta bort (1-#{min(sticks[heap_to_remove - 1], 3)})?:"
+                puts "Fel antal pinnar. Man måste ta bort ett ett antal mellan 1-#{min(sticks[heap_to_remove - 1], 3)}."
                 sticks_to_remove = gets.chomp.to_i
             end
         end
@@ -205,27 +206,64 @@ def pve(sticks, heaps, name_1, name_2, starting_player)
         end
         if sticks_left == false
             system("cls")
-            puts "VINNARER ÄR: #{starting_player}"
+            puts "VINNAREN ÄR: #{starting_player}"
             break
         end
     end
 end
 
-def pro_computer(sticks)
+# Beskrivning: Funktionen pro_computer är avsedd att hjälpa datorn att fatta beslut under spelet Nim. Genom att loopa genom högarna av tändstickor bestämmer funktionen vilken hög som är bäst att ta från baserat på en otroligt komplex och utvecklad strategi. Datorn väljer sedan antalet tändstickor att ta från den valda högen med hänsyn till olika villkor, såsom att minimera motståndarens möjligheter. Slutligen returnerar funktionen en array som innehåller den valda högen och antalet tändstickor som tas bort. På detta sätt kan datorn effektivt delta i spelet och försöka optimera sina chanser att vinna mot en mänsklig spelare.
+# Argument 1: Array - arrayen består av tal som representerar antalet pinnar i varje hög
+# Return: Array - arrayen består av två element, där första är vilken hög som datorn ska välja från och det andra elementet representerar hur många pinnar som ska tas bort från den högen
+# Exempel:
+# p pro_computer([9, 7, 5, 3, 1]) # => [4, 2]
+# p pro_computer([9, 7, 5, 1, 1]) # => [3, rand(1..3)]
+# p pro_computer([2, 7, 5, 3, 1]) # => [1, 1]
+# p pro_computer([1, 1, 1, 1]) # => [4, 1]
+# p pro_computer([9.9, 7.3, 5.3, 3.3, 1.3]) # => [5, 0.3] - Det fungerar med Floats, men man kommer aldrig stöta på det i spelet eftersom allt är Integers
+# p pro_computer("dahbudhau") # => Krash - går inte att köra funktionen med strängar eftersom man kan inte jämföra strängar med integers
+# p pro_computer([[3]]) # => Krash - går inte att köra funktionen med en array som består av arrayer
+# p pro_computer(["wad"]) # => Krash - går inte att köra funktionen med en array som består av strängar
+# Av: Max Svensby, Noah Saastad Bäckstrand
+# Datum: 2024-05-08
 
+def pro_computer(sticks)
     i = 0
+    heap_to_remove = 0
     while i < sticks.length
-        if sticks[i] <= 4 && sticks[i] > 1
+        if ((sticks[i] <= sticks[heap_to_remove]) && (sticks[i] > 1)) || (sticks[heap_to_remove] == 1)
             heap_to_remove = i
         end
         i += 1
     end
 
-    if 
+    if sticks[heap_to_remove] <= 4 && sticks[heap_to_remove] != 1
+        sticks_to_remove = (sticks[heap_to_remove] - 1)
+    elsif sticks[heap_to_remove] == 1
+        sticks_to_remove = 1
+    else
         sticks_to_remove = rand(1..3)
     end
-    
-    return heap_to_remove, sticks_to_remove
+
+    return (heap_to_remove + 1), sticks_to_remove
+end
+
+def min(n1, n2)
+    if n1 < n2
+        return n1
+    else
+        return n2
+    end
+end
+
+def god(answer)
+    if answer.downcase == "god" || answer.downcase == "gud" || answer.downcase == "g"
+        system("cls")
+        puts "Du förlorade"
+        puts "VINNAREN ÄR: Gud"
+        sleep(4)
+        exit
+    end
 end
 
 game()
