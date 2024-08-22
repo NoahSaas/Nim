8298c6e6296e4e3627c1424e87ffa6aae3b7f7cf
NoahSaas <151681167+NoahSaas@users.noreply.github.com>
2024-08-22 12:17:24 +0200

    .

--- del1.rb
+++ del1.rb
@@ -1,53 +1,22 @@
-def game()
-    puts "Skriv in namn på spelare 1:"
-    name_1 = gets.chomp
+def unqiue(array)
+    new_array = []
+    elements_to_check = []
 
-    puts "Skriv in namn på spelare 2:"
-    name_2 = gets.chomp
-
-    answer = nil
-
-    while answer != "n"
-        sticks = rand(11...17)
-
-        if rand(1...2) == 1
-            starting_player = name_1
-        else
-            starting_player = name_2
-        end
-
-        while sticks > 0
-            puts "#{starting_player}:s tur"
-
-            puts "Det finns #{sticks}st pinnar kvar, hur många vill du ta bort (1-3)?:"
-            sticks_to_remove = gets.chomp.to_i
-
-            while sticks_to_remove > 3 || sticks_to_remove < 1
-                puts "Det finns #{sticks}st pinnar kvar, hur många vill du ta bort (1-3)?:"
-                puts "Fel antal pinnar. Man måste ta bort ett ett antal mellan 1-3."
-                sticks_to_remove = gets.chomp.to_i
+    for i in 0...array.length()
+        if elements_to_check.length != 0
+            for j in 0...elements_to_check.length()
+                if array[i] != elements_to_check[j] || elements_to_check.length != 0
+                    new_array.append(array[i])
+                else
+                    elements_to_check.append(array[i])
+                end
             end
-
-            sticks -= sticks_to_remove
-
-            if starting_player == name_1
-                starting_player = name_2
-            else
-                starting_player = name_1
-            end
-
-            if sticks <= 0
-                puts "VINNARER ÄR: #{starting_player}"
-                break
-            end
-        end
-        puts "Vill ni spela igen? (Y/N)"
-        answer = gets.chomp.to_s.downcase
-        while answer != "n" && answer != "y"
-            puts "Svaret måste vara Y eller N"
-            answer = gets.chomp.to_s.downcase
         end
     end
+
+    return new_array
 end
 
-game()
+array1 = [0, 9, 2, 2]
+
+p unqiue(array1)
